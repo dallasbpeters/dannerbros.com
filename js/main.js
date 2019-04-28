ef39a513fb53573c02f5842eba9358cc2d109656
Dallas <dpeters@americanbible.org>
2019-04-27 22:07:31 -0500

    some more desktop styles

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $(function() {
+    var hero__header__navigation = $(".nav-default");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 30) {
+        hero__header__navigation
+          .removeClass("nav-default")
+          .addClass("nav-fixed");
+      } else {
+        hero__header__navigation
+          .removeClass("nav-fixed")
+          .addClass("nav-default");
+      }
+    });
+  });
+
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
